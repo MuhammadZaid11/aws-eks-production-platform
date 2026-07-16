7ea8aa101157c885b8ebe5d928074ee5eec4984b
MuhammadZaid11 <zaid.anwer.dev@gmail.com>
2026-07-16 22:18:53 +0500

    vpc iam and srg created with terraform

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -3,3 +3,21 @@ module "vpc" {
   source = "../modules/vpc"
 
 }
+
+module "iam" {
+
+  source = "../modules/iam"
+
+  project_name = "eks-platform"
+
+}
+
+module "security_groups" {
+
+  source = "../modules/security-groups"
+
+  project_name = "eks-platform"
+
+  vpc_id = module.vpc.vpc_id
+
+}
